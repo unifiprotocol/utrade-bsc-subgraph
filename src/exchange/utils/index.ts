468e57770b1b2b12d0bfbe8d83087afd9ea59dab
Daniel Blanco Parla <dblancoparla@gmail.com>
2021-06-23 11:57:49 +0200

    fix: Factory address update

--- src/exchange/utils/index.ts
+++ src/exchange/utils/index.ts
@@ -6,7 +6,7 @@ import { BEP20NameBytes } from "../../../generated/Factory/BEP20NameBytes";
 import { Factory as FactoryContract } from "../../../generated/templates/Pair/Factory";
 
 export let ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
-export let FACTORY_ADDRESS = "0xca143ce32fe78f1f7019d7d551a6402fc5350c73";
+export let FACTORY_ADDRESS = "0xA5Ba037Ec16c45f8ae09e013C1849554C01385f5";
 
 export let ZERO_BI = BigInt.fromI32(0);
 export let ONE_BI = BigInt.fromI32(1);
